74e6bf0396a24f2c2b919e5ee5c72885fa3100e9
Lewis Jang <lewis.aedes@gmail.com>
2021-11-01 21:14:00 +0800

    add entity

--- ember/_settings.py
+++ ember/_settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'people.apps.PeopleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- people/models.py
+++ people/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Entity(models.Model):
+    id = models.AutoField(primary_key=True)
+    display_name = models.CharField(max_length=30)
+    twitter_id = models.CharField(max_length=30)
+    connects = models.ManyToManyField("self", related_name="entity_connect", blank=True)
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    display_name = models.CharField(max_length=30)
