597ff4bb7cd1ac70da9436553abdb3c332291946
Lewis Jang <lewis.aedes@gmail.com>
2021-11-02 16:45:48 +0800

    Friendly display

--- people/models.py
+++ people/models.py
@@ -7,6 +7,9 @@ class Entity(models.Model):
     twitter_id = models.CharField(max_length=30)
     connects = models.ManyToManyField("self", related_name="entity_connect", blank=True)
 
+    def __str__(self):
+        return str(self.id) + " - " + self.display_name
+
 
 class Tag(models.Model):
     id = models.AutoField(primary_key=True)
