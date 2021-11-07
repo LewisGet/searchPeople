b4e97a7c2ab1102ef25f8ccd01c7ebb56acb0855
Lewis Jang <lewis.aedes@gmail.com>
2021-11-07 13:48:04 +0800

    fixed connects be auto backward connection

--- people/models.py
+++ people/models.py
@@ -5,7 +5,7 @@ class Entity(models.Model):
     id = models.AutoField(primary_key=True)
     display_name = models.CharField(max_length=30)
     twitter_id = models.CharField(max_length=30)
-    connects = models.ManyToManyField("self", related_name="entity_connect", blank=True)
+    connects = models.ManyToManyField("self", related_name="entity_connect", blank=True, symmetrical=False)
 
     def __str__(self):
         return str(self.id) + " - " + self.display_name
